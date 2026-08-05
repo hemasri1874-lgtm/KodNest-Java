43779ef8158a3fb75fe352afc2867aef547b3f8f
Hema Sri <hemasri1874@gmail.com>
2026-08-05 11:59:22 +0530

    SwitchProgram

---
+++ track/M02/T01/SwitchMain.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class SwitchMain {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        switch(num) {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+    }
+}
