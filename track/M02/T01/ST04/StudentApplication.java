e4cc8a984a1015d7c5b363eb58846e4df7c03a62
Hema Sri <hemasri1874@gmail.com>
2026-08-12 13:00:11 +0530

    MethodOverloading

--- track/M02/T01/ST04/StudentApplication.java
+++ track/M02/T01/ST04/StudentApplication.java
@@ -1,22 +1,26 @@
-class StudentApplication{
-    public static void main(String[] args){
+
+class StudentApplication {
+
+    public static void main(String[] args) {
         Student s1 = new Student();
-        s1.input("Raja",18,5.5);
+        s1.input("Raja", 18, 5.5);
         s1.display();
     }
 }
-class Student{
+
+class Student {
+
     String name;
     int age;
     double height;
 
-    void input(String name, int age, double height){
+    void input(String name, int age, double height) {
         this.name = name;
         this.age = age;
         this.height = height;
     }
 
-    void display(){
+    void display() {
         System.out.println(name);
         System.out.println(age);
         System.out.println(height);
