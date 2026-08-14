c2d78f31c1153c5200bc8b2aafd179b4858be94b
Hema Sri <hemasri1874@gmail.com>
2026-08-14 11:49:00 +0530

    Constructors

---
+++ track/M02/T06/StudentApplication.java
@@ -0,0 +1,24 @@
+class StudentApplication{
+    public static void main(String[] args){
+        Student s1 = new Student("Raja",18,4.5);
+        s1.display();
+    }
+}
+
+class Student{
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height){
+        this.name=name;
+        this.age=age;
+        this.height=height;
+    }
+
+    void display(){
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+    }
+}
